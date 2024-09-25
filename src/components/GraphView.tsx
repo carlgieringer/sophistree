eeb30c850be312902a3d0d1a63db75a07b75960e
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2024-09-25 09:41:41 -1000

    Describe panToNodes improvements

--- src/components/GraphView.tsx
+++ src/components/GraphView.tsx
@@ -104,7 +104,20 @@ export default function GraphView({ id, style }: GraphViewProps) {
    * Pan the graph to include the given nodes. Currently just centers
    * on the nodes. I'd prefer to pan the minimum amount to include the
    * nodes (with some padding) and also decrease the zoom as necessary to
-   * encompass all the nodes, but I couldn't figure out how to do that.
+   * encompass all the nodes, but I couldn't figure out how to do that. The
+   * algorithm could go like:
+   *
+   * - Calculate the bounding box of the nodes with padding.
+   * - if the view already contains the bounding box, return.
+   * - Otherwise, find the first corner of the view that needs to be moved
+   *   to encompass the boundign box. Update the view to move the corner
+   *   to match the corresponding corner of the bounding box without affecting
+   *   the zoom.
+   * - If any of the other 3 corners of the bounding box are outside the view,
+   *   update the zoom so that the view encompasses the bounding box.
+   *
+   * Animating that is tricky because it requires zooming around a point
+   * intermediate of the current center and the desired center.
    */
   const panToNodes = useCallback((nodeIds: string[]) => {
     const cy = cyRef.current;
