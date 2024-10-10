8c09394bc8e2274603941daa9b92988190fb1d08
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2024-10-09 23:21:18 -0700

    WIP trying to narrow host permissions...but it doesn't work

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,18 @@
 import { wrapCallback } from "./extension/callbacks";
 import { accessChromeUrlErrorMessage } from "./extension/errorMessages";
-import { CreateMediaExcerptMessage } from "./extension/messages";
+import {
+  CreateMediaExcerptMessage,
+  RequestUrlMessage,
+} from "./extension/messages";
 import * as appLogger from "./logging/appLogging";
 
 chrome.runtime.onInstalled.addListener(
   wrapCallback(installContentScriptsInOpenTabs),
 );
 chrome.runtime.onInstalled.addListener(wrapCallback(installContextMenus));
+chrome.tabs.onActivated.addListener(
+  wrapCallback(installContentScriptIfNecessary),
+);
 chrome.contextMenus.onClicked.addListener(wrapCallback(handleContextMenuClick));
 void chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 
@@ -18,65 +24,97 @@ const addToSophistreeContextMenuTitle =
   process.env.NODE_ENV === "production" ? "+ Sophistree" : "+ Sophistree (Dev)";
 
 async function installContentScriptsInOpenTabs() {
+  try {
+    for (const tab of await chrome.tabs.query({})) {
+      if (!tab.id) {
+        continue;
+      }
+      await installContentScriptsInTab(tab.id);
+    }
+  } catch (error) {
+    appLogger.error("Failed to install content scripts in open tabs", error);
+  }
+}
+
+async function installContentScriptIfNecessary(
+  activeInfo: chrome.tabs.TabActiveInfo,
+) {
+  const { tabId } = activeInfo;
+  if (!(await isContentScriptInstalled(tabId))) {
+    await installContentScriptsInTab(tabId);
+  }
+}
+
+async function isContentScriptInstalled(tabId: number) {
+  const message: RequestUrlMessage = {
+    action: "requestUrl",
+  };
+  try {
+    const url: string | undefined = await chrome.tabs.sendMessage(
+      tabId,
+      message,
+    );
+    return url !== undefined;
+  } catch {
+    return false;
+  }
+}
+
+async function installContentScriptsInTab(tabId: number) {
   const manifest = chrome.runtime.getManifest();
   const contentScripts = manifest.content_scripts;
   if (!contentScripts) {
     return;
   }
   for (const cs of contentScripts) {
-    for (const tab of await chrome.tabs.query({})) {
-      if (!tab.id) {
-        continue;
-      }
-      const target = { tabId: tab.id, allFrames: cs.all_frames };
-      if (cs.js) {
-        try {
-          await chrome.scripting.executeScript({
-            files: cs.js,
-            injectImmediately: cs.run_at === "document_start",
-            world: "world" in cs ? cs.world : undefined,
-            target,
-          });
-        } catch (e) {
-          if (
-            e instanceof Error &&
-            e.message.includes(accessChromeUrlErrorMessage)
-          ) {
-            // Ignore this error, it's expected when trying to inject into chrome:// URLs.
-            // We don't request tabs permission to be able to limit which tabs we process.
-            continue;
-          } else {
-            appLogger.warn(
-              `Failed to executeScript [${cs.js.join(",")}] in tab ID ${tab.id} URL: ${tab.url}`,
-              e,
-            );
-          }
+    const target = { tabId, allFrames: cs.all_frames };
+    if (cs.js) {
+      try {
+        await chrome.scripting.executeScript({
+          files: cs.js,
+          injectImmediately: cs.run_at === "document_start",
+          world: "world" in cs ? cs.world : undefined,
+          target,
+        });
+      } catch (e) {
+        if (
+          e instanceof Error &&
+          e.message.includes(accessChromeUrlErrorMessage)
+        ) {
+          // Ignore this error, it's expected when trying to inject into chrome:// URLs.
+          // We don't request tabs permission to be able to limit which tabs we process.
+          continue;
+        } else {
+          appLogger.warn(
+            `Failed to executeScript [${cs.js.join(",")}] in tab ID ${tabId} `,
+            e,
+          );
         }
       }
-      if (cs.css) {
-        try {
-          await chrome.scripting.insertCSS({
-            files: cs.css,
-            origin:
-              "origin" in cs
-                ? (cs.origin as chrome.scripting.StyleOrigin)
-                : undefined,
-            target,
-          });
-        } catch (e) {
-          if (
-            e instanceof Error &&
-            e.message.includes(accessChromeUrlErrorMessage)
-          ) {
-            // Ignore this error, it's expected when trying to inject into chrome:// URLs.
-            // We don't request tabs permission to be able to limit which tabs we process.
-            continue;
-          } else {
-            appLogger.warn(
-              `Failed to insertCSS [${cs.css.join(",")}] in tab ID ${tab.id} URL: ${tab.url}`,
-              e,
-            );
-          }
+    }
+    if (cs.css) {
+      try {
+        await chrome.scripting.insertCSS({
+          files: cs.css,
+          origin:
+            "origin" in cs
+              ? (cs.origin as chrome.scripting.StyleOrigin)
+              : undefined,
+          target,
+        });
+      } catch (e) {
+        if (
+          e instanceof Error &&
+          e.message.includes(accessChromeUrlErrorMessage)
+        ) {
+          // Ignore this error, it's expected when trying to inject into chrome:// URLs.
+          // We don't request tabs permission to be able to limit which tabs we process.
+          continue;
+        } else {
+          appLogger.warn(
+            `Failed to insertCSS [${cs.css.join(",")}] in tab ID ${tabId}`,
+            e,
+          );
         }
       }
     }
